473f1791d26065c69d0f0f62638f2be1b683e23e
Taufan Augusta <taufan.augusta@protonmail.com>
2021-01-21 11:35:30 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'Services.dart';
 
 void main() {
   runApp(
@@ -36,7 +37,7 @@ class _MyAppState extends State<MyApp> {
             decoration: InputDecoration.collapsed(hintText: 'enter new task'),
           ),
           MaterialButton(
-            onPressed: () => addTodo(todoctrl.text),
+            onPressed: () => Services.addTodo(todoctrl.text),
             child: Text('Add'),
           ),
         ],
@@ -49,16 +50,5 @@ class _MyAppState extends State<MyApp> {
     var data = {'todoctrl': todoctrl};
     var response = http.post(url, body: data);
     print(response.toString());
-  }
-
-  Future<void> addTodo(String todoctrl) async {
-    print(todoctrl.isEmpty);
-    try {
-      var url = "http://192.168.31.224/flutter/addtodo.php";
-      var map = Map<String, dynamic>();
-      map['todoctrl'] = todoctrl;
-      final response = await http.post(url, body: map);
-      print("addTodo >> Response:: ${response.body}");
-    } catch (e) {}
   }
 }
